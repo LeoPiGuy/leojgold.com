d10bab7cf7fb2b45cf0b69326992f1730771c8a7
Leo Gold <leopiguy@gmail.com>
2019-09-21 00:56:03 -0400

    First Commit - Began landing page and init project

--- src/router.js
+++ src/router.js
@@ -1,25 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import LandingPage from './views/LandingPage.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/landing'
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/landing',
+      name: 'landing',
+      component: LandingPage
+    },
+    {
+      path: '/home',
+      name: 'home',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/HomePage.vue')
     }
   ]
 })
