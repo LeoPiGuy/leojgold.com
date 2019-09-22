20daeb4eb98cb6cb719dd365de3866d9ab4417a5
Leo Gold <leopiguy@gmail.com>
2019-09-22 10:18:23 -0400

    attempt to resolve build issues 5

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/tailwind.css'
+import MainFooter from './components/MainFooter.vue'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  components: {
+    MainFooter
+  },
   render: h => h(App)
 }).$mount('#app')
