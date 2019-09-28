0340ea7902d6ae6a7c3ec8b3521abe73d3120063
Leo Gold <leopiguy@gmail.com>
2019-09-28 12:13:30 -0400

    Temporarily removed route

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -21,7 +21,7 @@
       <div class="choices">
         <p class="caption c1">I present you with some options. Choose wisely.</p>
         <div class="flex justify-center">
-          <router-link class="continue-button" to="/home"><span>Continue to My Website</span></router-link>
+          <router-link class="continue-button" to="/"><span>Continue to My Website</span></router-link>
         </div>
         <p class="text-gray-400 caption c2">or you can visit / contact me at any of the sites listed below:</p>
         <div class="flex justify-center flex-wrap">
