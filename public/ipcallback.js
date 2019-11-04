a54d1927d4b97b43b41977a22c198df84f79b565
Leo Gold <leopiguy@gmail.com>
2019-11-04 09:45:13 -0500

    Added a more accurate ip logging functionality

--- public/ipcallback.js
+++ public/ipcallback.js
@@ -1,9 +1,29 @@
+let mainIp = null
+let mainData = null
+
+// eslint-disable-next-line no-unused-vars
 function getIP (json) {
-  var data = JSON.stringify(json)
-  var ip = json.ip
+  mainIp = json.ip
+  if (mainData) {
+    console.log('ip 2nd')
+    postData()
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function getIpInfo (json) {
+  mainData = JSON.stringify(json)
+  if (mainIp) {
+    console.log('data first')
+    postData()
+  }
+}
+
+function postData () {
+  var ip = mainIp
   var xhr = new XMLHttpRequest()
   var link = 'https://us-central1-website-analytics-257923.cloudfunctions.net/logip?ip=' + ip
   xhr.open('POST', link)
   xhr.setRequestHeader('Content-Type', 'application/json')
-  xhr.send(data)
+  xhr.send(mainData)
 }
